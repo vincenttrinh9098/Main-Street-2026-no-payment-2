d6a1449d457bad0d6c5f2cfa2bc66f76b3c7ae4d
Vincent Trinh <vincenttrinh9098@gmail.com>
2025-12-28 22:42:37 -0800

    changes in home-page.js for images

--- home-page.js
+++ home-page.js
@@ -1,13 +1,13 @@
 const popularItems = [
-  { img: "/images/bagel/breakfast_ham.jpg", title: "Breakfast Ham Egg Cheese Bagel", desc: "Bagel with ham, egg, and cheese" },
-  { img: "/images/bagel/breakfast_bacon.jpg", title: "Breakfast Bacon Bagel", desc: "Bagel with crispy bacon and egg" },
-  { img: "/images/bagel/breakfast_sausage.jpg", title: "Breakfast Sausage Bagel", desc: "Bagel with savory sausage and egg" },
-  { img: "/images/sandwich/crossaint_ham.jpg", title: "Ham Croissant Sandwich", desc: "Flaky croissant with ham and cheese" },
-  { img: "/images/bagel/nova_lox.jpg", title: "Nova Lox Bagel", desc: "Bagel with smoked salmon and cream cheese" },
-  { img: "/images/sandwich/turkey_bacon_avacado_sandwich.jpg", title: "Turkey Bacon Avocado Sandwich", desc: "Sandwich with turkey, bacon, and avocado" },
-  { img: "/images/drinks/thai_tea_snow_smoothie.jpg", title: "Thai Tea Snow Smoothie", desc: "Iced Thai tea blended into a smoothie" },
-  { img: "/images/drinks/lava_flow_smoothie.jpg", title: "Lava Flow Smoothie", desc: "Strawberry and banana blended smoothie" },
-  { img: "/images/drinks/vietnamese_coffee.jpg", title: "Vietnamese Coffee", desc: "Rich and sweet iced coffee" }
+  { img: "images/bagel/breakfast_ham.jpg", title: "Breakfast Ham Egg Cheese Bagel", desc: "Bagel with ham, egg, and cheese" },
+  { img: "images/bagel/breakfast_bacon.jpg", title: "Breakfast Bacon Bagel", desc: "Bagel with crispy bacon and egg" },
+  { img: "images/bagel/breakfast_sausage.jpg", title: "Breakfast Sausage Bagel", desc: "Bagel with savory sausage and egg" },
+  { img: "images/sandwich/crossaint_ham.jpg", title: "Ham Croissant Sandwich", desc: "Flaky croissant with ham and cheese" },
+  { img: "images/bagel/nova_lox.jpg", title: "Nova Lox Bagel", desc: "Bagel with smoked salmon and cream cheese" },
+  { img: "images/sandwich/turkey_bacon_avacado_sandwich.jpg", title: "Turkey Bacon Avocado Sandwich", desc: "Sandwich with turkey, bacon, and avocado" },
+  { img: "images/drinks/thai_tea_snow_smoothie.jpg", title: "Thai Tea Snow Smoothie", desc: "Iced Thai tea blended into a smoothie" },
+  { img: "images/drinks/lava_flow_smoothie.jpg", title: "Lava Flow Smoothie", desc: "Strawberry and banana blended smoothie" },
+  { img: "images/drinks/vietnamese_coffee.jpg", title: "Vietnamese Coffee", desc: "Rich and sweet iced coffee" }
 ];
 
 
